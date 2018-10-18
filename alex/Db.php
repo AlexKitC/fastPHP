5701f61561e5e92989368b1651166b58adbbd4a2
Alex-黑白 <392999164@qq.com>
2018-10-18 09:42:44 +0800

    add functions about CURD for Db Class
    add functions about CURD for Db Class,better for Db

--- alex/Db.php
+++ alex/Db.php
@@ -7,10 +7,11 @@
 namespace Db;
 class Db{
 
-    private $conn;
+    private $conn;//连接句柄
     private $sql;
+    private $table;
 
-    public function __construct(){
+    public function __construct($table){
         if(!class_exists('pdo')){
             die('错误！当前环境未开启php_pdo_mysql扩展');
         }else{
@@ -18,6 +19,7 @@ class Db{
                 $conn = new \PDO("mysql:host=".SQLHOST.";dbname=".DATABASE,SQLUSERNAME,SQLPASSWORD,array(\PDO::MYSQL_ATTR_INIT_COMMAND => "set names utf8"));
                 if($conn){
                     $this -> conn = $conn;
+                    $this -> table = SQLTABLEPREFIX == ''?$table:SQLTABLEPREFIX.$table;
                 }
             }catch(\PDOException $e){
                 echo $e->getMessage();
@@ -27,21 +29,19 @@ class Db{
 
     /**
      * 查询多个符合条件的条目 返回二维数组
-     * @param $table  表名字
      * @param $fields 查询的字段(仅支持数组)
      * @param $where  查询条件(条件)
      * @param $limit  查询的条数
      * @param $orderBy 排序
      */
-    public function query($table,$fields="*",$where='',$limit='',$orderBy=""){
-        $table = SQLTABLEPREFIX == ''?$table:SQLTABLEPREFIX.$table;
+    public function query($fields="*",$where='',$limit='',$orderBy=""){
         $where = $where == ''?"":" WHERE ".$where;
         $limit = $limit == ''?"":" LIMIT ".$limit;
         $orderBy = $orderBy == ""?"":" ORDER BY ".$orderBy;
         $sql = "";
         if(is_array($fields)){
             $filedsStr = implode(",",$fields);
-            $sql = "SELECT ".$filedsStr." FROM ".$table.$where.$limit.$orderBy;
+            $sql = "SELECT ".$filedsStr." FROM ".$this -> table.$where.$limit.$orderBy;
             $this -> sql = $sql;
             $tmp_cost = microtime(true);
             $res = $this -> conn -> prepare($sql);
@@ -56,16 +56,14 @@ class Db{
 
     /**
      * 查询单个条目 返回一维数组
-     * @param $table  表名
      * @param $fields 查询的字段(仅支持数组)
      * * @param $where  查询条件(条件)
      */
-    public function find($table,$fields,$where=''){
-        $table = SQLTABLEPREFIX == ''?$table:SQLTABLEPREFIX.$table;
+    public function find($fields,$where=''){
         $where = $where == ''?"":" WHERE ".$where;
         if(is_array($fields)){
             $filedsStr = implode(",",$fields);
-            $sql = "SELECT ".$filedsStr." FROM ".$table.$where." LIMIT 1";
+            $sql = "SELECT ".$filedsStr." FROM ".$this -> table.$where." LIMIT 1";
             $this -> sql = $sql;
             $tmp_cost = microtime(true);
             $res = $this -> conn -> prepare($sql);
@@ -81,11 +79,9 @@ class Db{
 
     /**
      * 获取条目
-     * @param $table 表名
      */
-    public function getCount($table){
-        $table = SQLTABLEPREFIX == ''?$table:SQLTABLEPREFIX.$table;
-        $sql = "SELECT count('*') FROM ".$table;
+    public function getCount(){
+        $sql = "SELECT count('*') FROM ".$this -> table;
         $this -> sql = $sql;
         $tmp_cost = microtime(true);
         $result = $this -> conn -> query($sql);
@@ -95,11 +91,9 @@ class Db{
 
     /**
      * 插入单个条目 返回受影响的行
-     * @param $table  表名
      * @param $fields 插入的字段(仅支持数组)
      */
-    public function insert($table,$fields){
-        $table = SQLTABLEPREFIX == ''?$table:SQLTABLEPREFIX.$table;
+    public function insert($fields){
         $fields_keys = array_keys($fields);
         $fields_vals = array_values($fields);
         $pre = "";
@@ -116,7 +110,7 @@ class Db{
                 $aft .= ",?)";
             }
         }
-        $sql = "INSERT INTO ".$table.$pre.$aft;
+        $sql = "INSERT INTO ".$this -> table.$pre.$aft;
         $stmt = $this -> conn -> prepare($sql);
         foreach($fields_vals as $k => $v){
             $stmt -> bindParam($k+1,$fields_vals[$k]);
@@ -124,6 +118,45 @@ class Db{
         $res = $stmt -> execute();
         return $res;
     }
+
+    /**
+     * 更新某一条数据的某几个字段
+     * @param $fields 待更新字段数组 
+     * @param $where 满足的条件
+     */
+    public function update($fields,$where){
+        $fields_keys = array_keys($fields);
+        $fields_vals = array_values($fields);
+        $pre = "";
+        foreach($fields_keys as $k => $v){
+            if($k == 0){
+                $pre = " SET ".$fields_keys[$k]." = ?";
+            }elseif($k < count($fields_keys)-1){
+                $pre .= ",".$fields_keys[$k]." = ?";
+            }elseif($k == count($fields_keys)-1){
+                $pre .= ",".$fields_keys[$k]." = ?";
+            }   
+        }
+        $sql = "UPDATE ".$this -> table.$pre." WHERE ".$where;
+        $stmt = $this -> conn -> prepare($sql);
+        foreach($fields_vals as $k => $v){
+            $stmt -> bindParam($k+1,$fields_vals[$k]);
+        }
+        $res = $stmt -> execute();
+        return $res;
+    }
+
+    /**
+     * 删除满足条件的行
+     * @param $where 条件
+     * @return 受影响的行数
+     */
+    public function delete($where){
+        $res = $this -> conn -> exec("DELETE FROM ".$this -> table." WHERE ".$where);
+        return $res;
+    }
+
+    
 
     /**
      * 数据库行为日志记录
