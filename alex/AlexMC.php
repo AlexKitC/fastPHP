f6ad0c16000a6bdff9bfacf221ff7b046efdb834
Alex-黑白 <392999164@qq.com>
2018-11-20 10:45:02 +0800

    add redis file
    add redis file dec code

--- alex/AlexMC.php
+++ alex/AlexMC.php
@@ -18,13 +18,15 @@ if(VIEW_ENGINE_TURN == 'TRUE'){
     die("ERROR: class View() need config.php  VIEW_ENGINE_TURN  => 'TRUE'");
 }
 
+if(REDIS_ENGINE_TURN == 'TRUE'){
+    require_once APP_PATH."/extends/redis/Redis.php";
+}else{
+    die("ERROR: class Redis() need config.php  REDIS_ENGINE_TURN  => 'TRUE'");
+}
+
 class AlexMC{
 
     public function go(){
-        // spl_autoload_register(array($this, 'loadClass'));
-        // $this->setReporting();
-        // $this->removeMagicQuotes();
-        // $this->unregisterGlobals();
         $this->callHook();
     }
 
@@ -108,19 +110,7 @@ class AlexMC{
             header('Location: '.$_SERVER['SCRIPT_NAME'].$PATH_INFO);
         }
 
-        // 数据为空的处理
-        // $action = empty($action) ? 'index' : $action;
-        // $queryString  = empty($queryString) ? array() : $queryString;
- 
-        // // 实例化控制器
-        // $int = new $controller($controllerName, $action);
- 
-        // // 如果控制器存和动作存在，这调用并传入URL参数
-        // if ((int)method_exists($controller, $action)) {
-        //     call_user_func_array(array($int, $action), $queryString);
-        // } else {
-        //     exit($controller . "控制器不存在");
-        // }
+        
     }
 
     /**
@@ -161,68 +151,13 @@ class AlexMC{
         }
     }
  
-    // 检测开发环境
-    // function setReporting() {
-    //     if (APP_DEBUG == true) {
-    //         error_reporting(E_ALL);
-    //         ini_set('display_errors','On');
-    //     } else {
-    //         error_reporting(E_ALL);
-    //         ini_set('display_errors','Off');
-    //         ini_set('log_errors', 'On');
-    //         ini_set('error_log', RUNTIME_PATH. 'logs/error.log');
-    //     }
-    // }
+    
  
-    // 删除敏感字符
-    // function stripSlashesDeep($value) {
-    //     $value = is_array($value) ? array_map('stripSlashesDeep', $value) : stripslashes($value);
-    //     return $value;
-    // }
+    
  
-    // 检测敏感字符并删除 
-    // function removeMagicQuotes() {
-    //     if ( get_magic_quotes_gpc() ) {
-    //         $_GET = stripSlashesDeep($_GET );
-    //         $_POST = stripSlashesDeep($_POST );
-    //         $_COOKIE = stripSlashesDeep($_COOKIE);
-    //         $_SESSION = stripSlashesDeep($_SESSION);
-    //     }
-    // }
+    
  
-    // 检测自定义全局变量（register globals）并移除
-    // function unregisterGlobals() {
-    //     if (ini_get('register_globals')) {
-    //         $array = array('_SESSION', '_POST', '_GET', '_COOKIE', '_REQUEST', '_SERVER', '_ENV', '_FILES');
-    //        foreach ($array as $value) {
-    //             foreach ($GLOBALS[$value] as $key => $var) {
-    //                 if ($var === $GLOBALS[$key]) {
-    //                     unset($GLOBALS[$key]);
-    //                 }
-    //             }
-    //         }
-    //     }
-    // }
- 
-    //自动加载控制器和模型类 
-    // static function loadClass($class) {
-    //     $frameworks = ROOT . $class . EXT;
-    //     $controllers = APP_PATH . 'application/controller/' . $class . EXT;
-    //     $models = APP_PATH . 'application/models/' . $class . EXT;
- 
-    //     if (file_exists($frameworks)) {
-    //         // 加载框架核心类
-    //         include $frameworks;
-    //     } elseif (file_exists($controllers)) {
-    //         // 加载应用控制器类
-    //         include $controllers;
-    //     } elseif (file_exists($models)) {
-    //         //加载应用模型类
-    //         include $models;
-    //     } else {
-    //         /* 错误代码 */
-    //     }
-    // }
+    
 
 
 }
