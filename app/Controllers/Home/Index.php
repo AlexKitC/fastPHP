aa084acfea6cf1e45cb6dc6acceb56d40f085de5
lilin <lilin@shanshantao.com>
2019-10-08 17:22:34 +0800

    animate

--- app/Controllers/Home/Index.php
+++ app/Controllers/Home/Index.php
@@ -7,12 +7,11 @@ class Index extends Controller
     
     public function welcome() 
     {
-        // Log::getInstance()->info(__FUNCTION__.'aaa!');
-        // $arr = \App\Models\TestModel::test();
-        // dd($arr);
-        // $page = isset($_GET['page']) ? $_GET['page'] : 0;
-        // $arr = array_splice($arr,4*$page,4);
-        // $this->simplePaginate(4);
-        show('home/index/welcome');
+        show('home/index/welcome', [
+            'app_name' => APP_NAME,
+            'author'   => 'Alex-黑白',
+            'qq'       => '392999164',
+            'version'  => APP_VERSION
+        ]);
     }
 }
